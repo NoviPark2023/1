534516dd8c1c2ac62bf4bc43b76fe456a4118d3e
datatab <dejan@acreno.rs>
2021-10-24 10:19:52 +0200

    Addedd Login View

--- config/settings/base.py
+++ config/settings/base.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
