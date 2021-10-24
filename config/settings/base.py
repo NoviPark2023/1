301c025a61cea242d4ceb206ecab6277a2b98515
datatab <dejan@acreno.rs>
2021-10-24 10:21:39 +0200

    Addedd Login View

--- config/settings/base.py
+++ config/settings/base.py
@@ -180,9 +180,9 @@ MEDIA_URL = "/media/"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000'
+# ]
 
 # DOCS
 # ------------------------------------------------------------------------------
