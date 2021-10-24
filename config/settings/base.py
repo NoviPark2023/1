09daedfa0032308462437e8ba56b0520a2efc248
datatab <dejan@acreno.rs>
2021-10-24 06:21:29 +0200

    Start TESTING

--- config/settings/base.py
+++ config/settings/base.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # STATIC
 # ------------------------------------------------------------------------------
 STATIC_ROOT = str(BASE_DIR / "static")  # In production we want to use CDN
-STATIC_URL = "/static/"
+STATIC_URL = "/static/rest_framework/"
 STATICFILES_DIRS = [str(APPS_DIR / "static")]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
