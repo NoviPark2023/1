e59226f942a4a0c9cf5708bead6a73a61d868626
Dejan Cugalj Fww <absolute_url>
2021-10-25 11:07:34 +0200

    Changed CORD WHITE LIST

--- real_estate_api/kupci/models.py
+++ real_estate_api/kupci/models.py
@@ -9,7 +9,7 @@ class Kupci(models.Model):
     id_kupca = models.BigAutoField(primary_key=True)
     lice = models.CharField(max_length=20,
                             choices=STATUS_LICA,
-                            default='Fizicko Lice')
+                            default='Fizicko Lice',)
     ime_prezime = models.CharField('Ime i prezime Kupca', max_length=50)
     email = models.EmailField('Email Kupca')
     broj_telefona = models.CharField('Broj telefona', max_length=20)
