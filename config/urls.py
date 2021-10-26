a2b195a97ff805f3eefa97736e0b45da8cc04c11
Dejan Cugalj Fww <absolute_url>
2021-10-26 10:31:43 +0200

    Fixed URLs in config

--- config/urls.py
+++ config/urls.py
@@ -8,30 +8,34 @@ from django.conf import settings
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from rest_framework.documentation import include_docs_urls
 
-
+# Dokumentacija inicijalna podesavanja polje u zaglavlju
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=False,
-   permission_classes=(permissions.BasePermission,),
+    openapi.Info(
+        title="FACTORY Real Estate API Managment",
+        default_version='v0.1',
+        description="Dokumantacija i upravljanje API putanjama na projektu 'Prodaja Stanova'.",
+        contact=openapi.Contact(name='@datatab', email="dejan.cugalj@factoryww.com"),
+        license=openapi.License(name="GNU General Public License v3"),
+    ),
+    public=False,
+    permission_classes=(permissions.BasePermission,),
 )
 
-
 urlpatterns = [
+    # Root Super Admin Path
     path('admin/', admin.site.urls),
+    # Root LogIn home path
     path('', LoginView.as_view(template_name='./login-home.html'), name='login'),
+    # Root Putanja do Kupaca
     path('kupci/', include('real_estate_api.kupci.urls', namespace='kupci')),
+    # Root Putanja do Korisnika
     path('korisnici/', include('real_estate_api.korisnici.urls', namespace='korisnici')),
+    # Root Putanja do Stanova
     path('stanovi/', include('real_estate_api.stanovi.urls', namespace='stanovi')),
+    # Root Putanja do Ponuda
     path('ponude/', include('real_estate_api.ponude.urls', namespace='ponude')),
+    # Root Putanja do Dokumentacije
     url(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='docs'),
 ]
 
