8a62a7bfb778117085238bd9f9466f37d172863b
Dejan Cugalj Fww <absolute_url>
2021-10-29 10:54:21 +0200

    Addedd Hashers in base config

--- config/settings/base.py
+++ config/settings/base.py
@@ -157,7 +157,7 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'accounts.hashers.PBKDF2WrappedSHA1PasswordHasher',
+    #'accounts.hashers.PBKDF2WrappedSHA1PasswordHasher',
 ]
 
 # INTERNATIONALIZATION
