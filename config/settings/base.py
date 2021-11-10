e3e7f1bc91e33bc45c96fc3cba75cb48e3616b5f
Dejan Cugalj Fww <absolute_url>
2021-11-10 10:33:54 +0100

    Addedd CROS auth options

--- config/settings/base.py
+++ config/settings/base.py
@@ -186,13 +186,32 @@ STATICFILES_FINDERS = [
 MEDIA_ROOT = str(BASE_DIR / "media")
 MEDIA_URL = "/media/"
 
-CORS_ORIGIN_ALLOW_ALL = True
-
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
     'http://api.dejan.pro',
     'http://localhost:3000',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1',
 ]
-
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 # DOCS
 # ------------------------------------------------------------------------------
 REST_FRAMEWORK = {
