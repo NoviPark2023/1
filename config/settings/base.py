82054b44104fff75942698b9965fc0717fb94362
datatab <dejan@acreno.rs>
2021-11-20 01:26:00 +0100

    Add  CORS_ORIGIN_WHITELIST 'https://stanovi.dejan.pro'

--- config/settings/base.py
+++ config/settings/base.py
@@ -192,6 +192,8 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://127.0.0.1:8000',
     'http://127.0.0.1',
+    '164.92.253.160',
+    'https://stanovi.dejan.pro',
 ]
 CORS_ALLOW_METHODS = [
     'DELETE',
