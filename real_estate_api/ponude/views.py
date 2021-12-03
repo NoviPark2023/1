35ccd6a0ad443f850d76303007c2040d37d30865
datatab <dejan@acreno.rs>
2021-12-03 23:38:03 +0100

    Implement automatic set Odobrenje fields

--- real_estate_api/ponude/views.py
+++ real_estate_api/ponude/views.py
@@ -1,8 +1,11 @@
+from django.db.backends.ddl_references import Table
+from django.db.models import Case, When, Value, F
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
 
 from .models import Ponude
 from .serializers import PonudeSerializer
+from ..stanovi.models import Stanovi
 
 lookup_field = 'id_ponude'
 lookup_field_stan = 'id_stana'
@@ -60,6 +63,30 @@ class UrediPonuduViewAPI(generics.RetrieveUpdateAPIView):
     lookup_field = lookup_field
     queryset = Ponude.objects.all()
     serializer_class = PonudeSerializer
+
+    def put(self, request, *args, **kwargs):
+        """
+        Ukoliko se cena Stana razlikuje od cene Stana u Ponudi, automatski se polje
+        modela u Ponudama setuje na True ili False.
+
+        * True = Kada se cena razlikuje
+        * False = Kada je cena ista
+
+        :param request: Ceo Objekat Ponue u responsu
+        :param kwargs: Vraca ID Ponude
+        :return: partial_update
+        """
+        stan = Stanovi.objects.get(id_stana__exact=request.data['stan'])
+        ponuda = Ponude.objects.get(id_ponude__exact=kwargs['id_ponude'])
+
+        if stan.cena_stana == int(request.data['cena_stana_za_kupca']):
+            ponuda.odobrenje = False
+        else:
+            ponuda.odobrenje = True
+
+        stan.save()
+        ponuda.save()
+        return self.partial_update(request, *args, **kwargs)
 
 
 class ObrisiPonuduAPIView(generics.RetrieveDestroyAPIView):
