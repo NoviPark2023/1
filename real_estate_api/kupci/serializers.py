0f1bab6942b07f96f82d6ac1b1114b3df379c89f
Dejan Cugalj Fww <dejan.cugalj@factoryww.com>
2021-12-29 00:24:35 +0100

    [PS-72] VALIDACIJA POLJA KLIJENTI: -Validirana polja su:   * Ime i Prezime   * e-mail   * broj telefona   * JMBG/PIB

--- real_estate_api/kupci/serializers.py
+++ real_estate_api/kupci/serializers.py
@@ -70,22 +70,89 @@ class KupciSerializer(serializers.ModelSerializer):
             'lista_kupaca_url',
         )
 
-    def get_detalji_kupca_url(self, obj):
+    @staticmethod
+    def validate_ime_prezime(value):
+        """
+        Provera Emaila Klijenta (Kupca) da li je vec registrovan.
+        ---
+        :param value: ime_prezime
+        :return: ValidationError || ime_prezime
+        """
+        kupac = Kupci.objects.filter(ime_prezime=value)
+        if kupac:
+            raise serializers.ValidationError(
+                f"Klijent sa ovim imenom i prezimenom: {value}, je vec registrovan u sistemu."
+            )
+        return value
+
+
+    @staticmethod
+    def validate_email(value):
+        """
+        Provera Emaila Klijenta (Kupca) da li je vec registrovan.
+        ---
+        :param value: email
+        :return: ValidationError || email
+        """
+        kupac = Kupci.objects.filter(email=value)
+        if kupac:
+            raise serializers.ValidationError(
+                f"Klijent sa e-mailom: {value}, je vec registrovan u sistemu."
+            )
+        return value
+
+    @staticmethod
+    def validate_broj_telefona(value):
+        """
+        Provera Broja Telefona Klijenta (Kupca) da li je vec registrovan.
+        ---
+        :param value: broj_telefona
+        :return: ValidationError || broj_telefona
+        """
+        kupac = Kupci.objects.filter(broj_telefona=value)
+        if kupac:
+            raise serializers.ValidationError(
+                f"Klijent sa tel. brojem: {value}, je vec registrovan u sistemu."
+            )
+        return value
+
+    @staticmethod
+    def validate_Jmbg_Pib(value):
+        """
+        Provera Jmbg_Pib-a Klijenta (Kupca) da li je vec registrovan.
+        ---
+        :param value: Jmbg_Pib
+        :return: ValidationError || Jmbg_Pib
+        """
+        kupac = Kupci.objects.filter(Jmbg_Pib=value)
+        if kupac:
+            raise serializers.ValidationError(
+                f"Klijent sa Jmbg_Pib-om: {value}, je vec registrovan u sistemu."
+            )
+        return value
+
+
+    @staticmethod
+    def get_detalji_kupca_url(obj):
         """Prosledi API putanju do detalji kupca"""
         return reverse("kupci:detalji_kupca", args=[obj.pk])
 
-    def get_izmeni_kupca_url(self, obj):
+    @staticmethod
+    def get_izmeni_kupca_url(obj):
         """Prosledi API putanju do uredi kupca"""
         return reverse("kupci:izmeni_kupca", args=[obj.pk])
 
-    def get_obrisi_kupca_url(self, obj):
+    @staticmethod
+    def get_obrisi_kupca_url(obj):
         """Prosledi API putanju do obrisi kupca"""
         return reverse("kupci:obrisi_kupca", args=[obj.pk])
 
-    def get_kreiraj_kupca_url(self, obj):
+    @staticmethod
+    def get_kreiraj_kupca_url(obj):
         return reverse("kupci:kreiraj_kupca")
 
-    def get_lista_kupaca_url(self, obj):
+    @staticmethod
+    def get_lista_kupaca_url(obj):
         """Prosledi API putanju do liste kupaca"""
 
         return reverse("kupci:lista_kupaca")
