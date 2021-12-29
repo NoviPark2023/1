96f35c0bfb517b80b09bac8ac507cfdd9809426d
Dejan Cugalj Fww <dejan.cugalj@factoryww.com>
2021-12-29 01:30:25 +0100

    [PS-72] VALIDACIJA POLJA PONUDE, STANOVI: -Validirana polja su PONUDE:   * broj_ugovora
    -Validirana polja su STANOVI:
     * lamela

--- real_estate_api/stanovi/models.py
+++ real_estate_api/stanovi/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 
 class Stanovi(models.Model):
-
     class OrijentacijaStana(models.TextChoices):
         SEVER = 'Sever', "Sever"
         JUG = 'Jug', "Jug"
@@ -17,24 +16,44 @@ class Stanovi(models.Model):
         PRODAT = 'prodat', "Prodat"
 
     id_stana = models.BigAutoField(primary_key=True)
-    lamela = models.CharField('Lamela', max_length=50, default='')
-    adresa_stana = models.CharField('Adresa stana', max_length=254, default='', blank=False, null=False)
+
+    lamela = models.CharField('Lamela',
+                              max_length=50,
+                              default='',
+                              unique=True
+                              )
+
+    adresa_stana = models.CharField('Adresa stana',
+                                    max_length=254,
+                                    default='',
+                                    blank=False,
+                                    null=False
+                                    )
+
     kvadratura = models.DecimalField('Kvadratura stana', max_digits=7, decimal_places=2)
+
     kvadratura_tri_od_sto = models.DecimalField('Korekcija kvadrature',
                                                 max_digits=7,
                                                 decimal_places=2,
                                                 default=0,
-                                                blank=True)
+                                                blank=True
+                                                )
+
     sprat = models.CharField('Sprat stana', max_length=10, default='1')
+
     broj_soba = models.FloatField('Broj soba stana', default=1)
+
     orijentisanost = models.CharField(max_length=20,
                                       choices=OrijentacijaStana.choices,
                                       default=OrijentacijaStana.JUG,
                                       blank=True,
                                       null=True)
     broj_terasa = models.PositiveIntegerField('Broj terasa stana', default=0)
+
     cena_stana = models.FloatField('Cena stana', default=0)
+
     napomena = models.CharField('Napomena', null=True, blank=True, max_length=250, default='')
+
     status_prodaje = models.CharField(max_length=20,
                                       choices=StatusProdaje.choices,
                                       default=StatusProdaje.DOSTUPAN)
