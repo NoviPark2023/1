c963dbd385490b9e60d33415c13bb8ee367f7bc6
Dejan Cugalj FWW <dejan.cugalj@factoryww.com>
2021-12-30 10:34:12 +0100

    [PS-57] Add and fix  * PG admin docker com.  * Pytest setup   * install pytest-django

--- real_estate_api/reports/test_reports/tests.py
+++ real_estate_api/reports/test_reports/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from real_estate_api.stanovi.models import Stanovi
+
+
+@pytest.mark.django_db
+def test_create_category():
+    category = Stanovi.objects.create(id_stana=1)
+    assert category.id_stana == 1
