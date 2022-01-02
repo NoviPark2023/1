29365c9a0a7571b048ae56ffbb9c6b6cda6159e8
Dejan Cugalj Fww <dejan.cugalj@factoryww.com>
2022-01-02 06:17:52 +0100

    [PS-81] Test Kupci

--- real_estate_api/kupci/tests_kupci/test_model_kupci_api.py
+++ real_estate_api/kupci/tests_kupci/test_model_kupci_api.py
@@ -8,11 +8,17 @@ fake = Faker()
 
 @pytest.mark.django_db
 def test_new_korisnik(kupci_factory):
+
     kupac = kupci_factory.create()
+
     print('\n')
+
     print(f'########### KUPAC IME PREZIME: {kupci_factory.ime_prezime}  #############################')
+
     print(f'########### KUPAC EMAIL: {kupci_factory.email}  #############################')
+
     print(f'##############  TELEFON KUPCA: {kupci_factory.broj_telefona}  ##########################')
+
     broj_kupaca_u_bazi = Kupci.objects.all().count()
     print(f'##############  COUNT: {broj_kupaca_u_bazi}  ##########################')
 
