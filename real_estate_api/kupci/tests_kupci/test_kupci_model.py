3834df9208e773ebe1970127d8d0cc35215822cd
Dejan Cugalj <dejan.cugalj@factoryww.com>
2022-01-08 13:45:43 +0000

    [PS-81] Test model 'Kupci'

---
+++ real_estate_api/kupci/tests_kupci/test_kupci_model.py
@@ -0,0 +1,200 @@
+from faker import Faker
+
+from real_estate_api.kupci.models import Kupci
+
+fake = Faker()
+
+
+class TestEntitetaKupci:
+    """Testitanje entiteta Kupci"""
+
+    def test_create_novi_kupac(self, novi_kupac_fixture):
+        """
+        Testiranje kreiranja novog Kupca i provera podataka nakon kreiranja.
+            * @see conftest.py
+
+        @param novi_kupac_fixture: entitet Kupci
+        """
+        kupac_from_db = Kupci.objects.all()
+        lice = kupac_from_db[0].lice
+        ime_prezime = kupac_from_db[0].ime_prezime
+        email = kupac_from_db[0].email
+        broj_telefona = kupac_from_db[0].broj_telefona
+        Jmbg_Pib = kupac_from_db[0].Jmbg_Pib
+        adresa = kupac_from_db[0].adresa
+
+        assert novi_kupac_fixture.lice == lice
+        assert novi_kupac_fixture.ime_prezime == ime_prezime
+        assert novi_kupac_fixture.email == email
+        assert novi_kupac_fixture.broj_telefona == broj_telefona
+        assert novi_kupac_fixture.Jmbg_Pib == Jmbg_Pib
+        assert novi_kupac_fixture.adresa == adresa
+
+    def test_kreiranje_novog_kupca(self, novi_kupac_fixture):
+        """
+        Test da li je samo jedan Kupac kreiran u bazi.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        assert Kupci.objects.all().count() == 1
+
+    def test_filter_ime_prezime(self, novi_kupac_fixture):
+        """
+        Testiranje Filtera po 'ime_prezime' polju.
+
+        @param novi_kupac_fixture: Kupci
+        """
+        assert Kupci.objects.filter(ime_prezime=novi_kupac_fixture.ime_prezime).exists()
+
+    def test_promeni_ime_prezime(self, novi_kupac_fixture):
+        """
+        Test Promena Imena i Prezimena, ali provo provera da li je u fixture ta vrednost.
+
+        @param novi_kupac_fixture: Kupci
+        """
+        # Proveri prvo da li je ime_prezime iz fixtura 'ime_prezime'
+        assert novi_kupac_fixture.ime_prezime == novi_kupac_fixture.ime_prezime
+
+        novi_kupac_fixture.ime_prezime = 'Slobodan Tomic'
+        novi_kupac_fixture.save()
+        movo_ime_kupca = Kupci.objects.get(ime_prezime="Slobodan Tomic")
+
+        assert movo_ime_kupca.ime_prezime == "Slobodan Tomic"
+
+    def test_filter_lice(self, novi_kupac_fixture):
+        """
+        Testiranje Filtera po 'lice' polju.
+
+        @param novi_kupac_fixture: Kupci
+        """
+        assert Kupci.objects.filter(lice=novi_kupac_fixture.lice).exists()
+
+    def test_promeni_lice(self, novi_kupac_fixture):
+        """
+        Test Promena polja 'lice', ali provo provera da li je u fixture ta vrednost.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        # Proveri prvo da li je lice iz fixtura 'FIZICKO'
+        assert novi_kupac_fixture.lice == "Fizicko"
+
+        novi_kupac_fixture.lice = 'Pravno'
+        novi_kupac_fixture.save()
+        movo_lice_kupca = Kupci.objects.get(lice="Pravno")
+        assert movo_lice_kupca.lice == "Pravno"
+
+    def test_filter_email(self, novi_kupac_fixture):
+        """
+        Testiranje Filtera po 'email' polju.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        assert Kupci.objects.filter(email=novi_kupac_fixture.email).exists()
+
+    def test_promeni_email(self, novi_kupac_fixture):
+        """
+        Test Promena polja 'email', ali provo provera da li je u fixture ta vrednost.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        # Proveri prvo da li je iz fixtura 'email'
+        assert novi_kupac_fixture.email == novi_kupac_fixture.email
+
+        novi_kupac_fixture.email = 'slobodan.tomic@factoryww.com'
+        novi_kupac_fixture.save()
+        movi_email_kupca = Kupci.objects.get(email="slobodan.tomic@factoryww.com")
+
+        assert movi_email_kupca.email == "slobodan.tomic@factoryww.com"
+
+    def test_filter_broj_telefona(self, novi_kupac_fixture):
+        """
+        Testiranje Filtera po 'broj_telefona' polju.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        assert Kupci.objects.filter(broj_telefona=novi_kupac_fixture.broj_telefona).exists()
+
+    def test_promeni_broj_telefona(self, novi_kupac_fixture):
+        """
+        Test Promena polja 'broj_telefona', ali provo provera da li je u fixture ta vrednost.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        # Proveri prvo da li je iz fixtura 'broj_telefona'
+        assert novi_kupac_fixture.broj_telefona == novi_kupac_fixture.broj_telefona
+
+        novi_kupac_fixture.broj_telefona = '+381 66 9878 99 88'
+        novi_kupac_fixture.save()
+        movi_broj_telefona_kupca = Kupci.objects.get(broj_telefona='+381 66 9878 99 88')
+
+        assert movi_broj_telefona_kupca.broj_telefona == '+381 66 9878 99 88'
+
+    def test_filter_Jmbg_Pib(self, novi_kupac_fixture):
+        """
+        Testiranje Filtera po 'Jmbg_Pib' polju.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        assert Kupci.objects.filter(Jmbg_Pib=novi_kupac_fixture.Jmbg_Pib).exists()
+
+    def test_promeni_Jmbg_Pib(self, novi_kupac_fixture):
+        """
+        Test Promena polja 'Jmbg_Pib', ali provo provera da li je u fixture ta vrednost.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        # Proveri prvo da li je iz fixtura 'Jmbg_Pib'
+        assert novi_kupac_fixture.Jmbg_Pib == novi_kupac_fixture.Jmbg_Pib
+
+        novi_kupac_fixture.Jmbg_Pib = '1110975800093'
+        novi_kupac_fixture.save()
+        movi_Jmbg_Pib_kupca = Kupci.objects.get(Jmbg_Pib='1110975800093')
+
+        assert movi_Jmbg_Pib_kupca.Jmbg_Pib == '1110975800093'
+
+    def test_filter_adresa(self, novi_kupac_fixture):
+        """
+        Testiranje Filtera po 'adresa' polju.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        assert Kupci.objects.filter(adresa=novi_kupac_fixture.adresa).exists()
+
+    def test_promeni_adresa(self, novi_kupac_fixture):
+        """
+        Test Promena polja 'adresa', ali provo provera da li je u fixture ta vrednost.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        # Proveri prvo da li je iz fixtura 'Jmbg_Pib'
+        assert novi_kupac_fixture.adresa == novi_kupac_fixture.adresa
+
+        novi_kupac_fixture.adresa = 'Preradovićeva 23'
+        novi_kupac_fixture.save()
+        movi_adresa_kupca = Kupci.objects.get(adresa='Preradovićeva 23')
+
+        assert movi_adresa_kupca.adresa == 'Preradovićeva 23'
+
+    def test_delete_kupca_iz_bazepodataka(self, novi_kupac_fixture):
+        """
+        Test brisanja Kupca iz baze podataka.
+
+        @param novi_kupac_fixture: Kupci
+        """
+
+        # Proveri prvo da li postoji kreiran Kupac u bazi.
+        assert Kupci.objects.all().count() == 1
+
+        Kupci.objects.get(id_kupca=novi_kupac_fixture.id_kupca).delete()
+
+        assert Kupci.objects.all().count() == 0
---
+++ real_estate_api/kupci/tests_kupci/test_kupci_serializer.py
@@ -0,0 +1,318 @@
+import json
+
+from rest_framework.reverse import reverse
+
+from conftest import fake
+from real_estate_api.kupci.models import Kupci
+
+
+class TestKupciSerijalizers:
+
+    def test_serializers_svi_kupci(self, client, nova_dva_kupaca_fixture, novi_autorizovan_korisnik_fixture):
+        """
+        Testiranje serijalizera za pregled svih Klijenata(Kupaca) sa fixturom od dva kreirana nova Kupca.
+        Testiranje se vrsi sa autorizovanim Korisnikom sistema.
+
+            * @see conftest.py (novi_autorizovan_korisnik_fixture)
+            * @see conftest.py (nova_dva_kupaca_fixture)
+
+        @param client: A Django test client instance.
+        @param nova_dva_kupaca_fixture: Kupci
+        @param novi_autorizovan_korisnik_fixture: Korisnik
+        """
+
+        response = client.get(reverse('kupci:lista_kupaca'))
+        print(f'RESPONSE: {response}')
+        print(f'RESPONSE CONTENT: {response.json()}')
+
+        kupci = Kupci.objects.all().order_by('id_kupca')
+        kupci_broj = Kupci.objects.all().count()
+
+        # Lokalno import 'KupciSerializer'.
+        from real_estate_api.kupci.serializers import KupciSerializer
+
+        serializer = KupciSerializer(kupci, many=True)
+
+        print(f'RESPONSE DATA: {json.dumps(serializer.data)}')
+        print(f'RESPONSE DATA: {json.dumps(response.json()["results"])}')
+
+        assert json.dumps(serializer.data) == json.dumps(response.json()["results"])
+        assert response.status_code == 200
+        assert kupci_broj == 2
+
+    def test_valid_serializers_detalji_jednog_kupca(self,
+                                                    client,
+                                                    nova_dva_kupaca_fixture,
+                                                    novi_autorizovan_korisnik_fixture
+                                                    ):
+        """
+        Testiranje serijalizera za pregled deatalja Klijenata(Kupaca) sa fixturom od dva kreirana nova Kupca.
+        Testiranje se vrsi sa autorizovanim Korisnikom sistema.
+
+            * @see conftest.py (novi_autorizovan_korisnik_fixture)
+            * @see conftest.py (nova_dva_kupaca_fixture)
+
+        @param client: A Django test client instance.
+        @param nova_dva_kupaca_fixture: Kupci
+        @param novi_autorizovan_korisnik_fixture: Korisnik
+        """
+
+        # First check how many Kupcaca in DB is *(should be 2)
+        broj_kupaca = Kupci.objects.all().count()
+        assert broj_kupaca == 2
+
+        # Get valid two Kupaca from Response
+        url_detalji_kupca_jedan = reverse('kupci:detalji_kupca', args=[nova_dva_kupaca_fixture[0].id_kupca])
+        url_detalji_kupca_dva = reverse('kupci:detalji_kupca', args=[nova_dva_kupaca_fixture[1].id_kupca])
+
+        response_kupac_jedan = client.get(url_detalji_kupca_jedan)
+        response_kupac_dva = client.get(url_detalji_kupca_dva)
+
+        kupac_jedan_from_response = Kupci.objects.get(id_kupca=nova_dva_kupaca_fixture[0].id_kupca)
+        kupac_dva_from_response = Kupci.objects.get(id_kupca=nova_dva_kupaca_fixture[1].id_kupca)
+
+        print(f'KUPAC ONE FROM RESPONSE: {kupac_jedan_from_response}')
+        print(f'KUPAC TWO FROM RESPONSE: {kupac_dva_from_response}')
+
+        # Lokalno import 'KupciSerializer'.
+        from real_estate_api.kupci.serializers import KupciSerializer
+
+        serializer_kupac_one = KupciSerializer(kupac_jedan_from_response)
+        serializer_kupac_two = KupciSerializer(kupac_dva_from_response)
+
+        assert json.dumps(serializer_kupac_one.data) == json.dumps(response_kupac_jedan.json())
+        assert json.dumps(serializer_kupac_two.data) == json.dumps(response_kupac_dva.json())
+
+    def test_invalid_serializers_detalji_jednog_kupca(self,
+                                                      client,
+                                                      nova_dva_kupaca_fixture,
+                                                      novi_autorizovan_korisnik_fixture
+                                                      ):
+        """
+        Testiranje serijalizera za pregled deatalja Klijenata(Kupaca) koji ne postoji
+        sa fixturom od dva kreirana nova Kupca.
+
+        Testiranje se vrsi sa autorizovanim Korisnikom sistema.
+
+            * @see conftest.py (novi_autorizovan_korisnik_fixture)
+            * @see conftest.py (nova_dva_kupaca_fixture)
+
+        @param client: A Django test client instance.
+        @param nova_dva_kupaca_fixture: Kupci
+        @param novi_autorizovan_korisnik_fixture: Korisnik
+        """
+
+        # Get invalid one Kupaca from Response
+        url_detalji_kupca_jedan = reverse('kupci:detalji_kupca', args=[nova_dva_kupaca_fixture[0].id_kupca + 1000])
+        response = client.get(url_detalji_kupca_jedan)
+        assert response.status_code == 404
+
+    def test_serializers_kreiraj_validnog_jednog_kupca(self,
+                                                       client,
+                                                       novi_autorizovan_korisnik_fixture
+                                                       ):
+        """
+        Testiranje serijalizera za kreiranje Klijenata(Kupaca) koji je validan,
+        sa fixturom od dva kreirana nova Kupca.
+
+        Testiranje se vrsi sa autorizovanim Korisnikom sistema.
+
+            * @see conftest.py (novi_autorizovan_korisnik_fixture)
+
+        @param client: A Django test client instance.
+        @param novi_autorizovan_korisnik_fixture: Korisnik
+        """
+        url_kreiraj_validnog_kupca = reverse('kupci:kreiraj_kupca')
+
+        # Podaci za izmenu Kupca
+        test_novi_kupac = json.dumps(
+            {
+                "lice": "Pravno",
+                "ime_prezime": fake.name(),
+                "email": fake.email(),
+                "broj_telefona": "+381 63 136 90 988",
+                "Jmbg_Pib": "0102030605",
+                "adresa": "Milenke Dravica 54"
+            }
+        )
+
+        response = client.post(url_kreiraj_validnog_kupca, data=test_novi_kupac, content_type='application/json')
+
+        assert response.status_code == 201
+
+    def test_serializers_kreiraj_nevalidnog_jednog_kupca(self,
+                                                         client,
+                                                         novi_autorizovan_korisnik_fixture
+                                                         ):
+        """
+        Testiranje serijalizera za kreiranje Klijenata(Kupaca) koji je nije validan *(status.HTTP_400_BAD_REQUEST),
+        sa fixturom od dva kreirana nova Kupca.
+
+        Testiranje se vrsi sa autorizovanim Korisnikom sistema.
+
+            * @see conftest.py (novi_autorizovan_korisnik_fixture)
+
+        @param client: A Django test client instance.
+        @param novi_autorizovan_korisnik_fixture: Korisnik
+        """
+        url_kreiraj_validnog_kupca = reverse('kupci:kreiraj_kupca')
+
+        # Podaci za izmenu Kupca
+        test_novi_kupac = json.dumps(
+            {
+                "lice_ne_validno": "Pravno",
+                "ime_prezime_ne_validno": fake.name(),
+                "email_ne_validno": fake.email(),
+                "broj_telefona_ne_validno": "+381 63 136 90 988",
+                "Jmbg_Pib_ne_validno": "0102030605",
+                "adresa_ne_validno": "Milenke Dravica 54"
+            }
+        )
+
+        response = client.post(url_kreiraj_validnog_kupca, data=test_novi_kupac, content_type='application/json')
+
+        assert response.status_code == 400
+
+    def test_serializers_kreiraj_validnog_jednog_kupca_401(self, client):
+        """
+        Testiranje serijalizera za kreiranje Klijenata(Kupaca) koji je validan,
+        sa fixturom od dva kreirana nova Kupca ali nije autorizovan.
+
+
+        @param client: A Django test client instance.
+        """
+        url_kreiraj_validnog_kupca = reverse('kupci:kreiraj_kupca')
+
+        # Podaci za izmenu Kupca
+        test_novi_kupac = json.dumps(
+            {
+                "lice": "Pravno",
+                "ime_prezime": fake.name(),
+                "email": fake.email(),
+                "broj_telefona": "+381 63 136 90 988",
+                "Jmbg_Pib": "0102030605",
+                "adresa": "Milenke Dravica 54"
+            }
+        )
+
+        response = client.post(url_kreiraj_validnog_kupca, data=test_novi_kupac, content_type='application/json')
+
+        assert response.status_code == 401
+
+    def test_serializers_izmeni_jednog_kupca_validan(self,
+                                                     client,
+                                                     novi_kupac_fixture,
+                                                     novi_autorizovan_korisnik_fixture
+                                                     ):
+        """
+        Test poziv 'kupci:izmeni_kupca' za API poziv Izmeni Kupca *(VALIDAN) sa autorizovanim Korisnikom.
+        Takodje se proverava i Response sadrzaj.
+
+            * @see conftest.py (novi_autorizovan_korisnik_fixture)
+            * @see conftest.py (novi_kupac_fixture)
+
+        @param client: A Django test client instance.
+        @param novi_autorizovan_korisnik_fixture: Korisnik.
+        @param novi_kupac_fixture: Klijent (Kupac).
+        """
+
+        # Podaci za izmenu Kupca
+        test_novi_kupac = json.dumps(
+            {
+                "lice": "Pravno",
+                "ime_prezime": fake.name(),
+                "email": fake.email(),
+                "broj_telefona": "+381 63 136 90 988",
+                "Jmbg_Pib": "0102030605",
+                "adresa": "Milenke Dravica 54"
+            }
+        )
+
+        url_izmeni_kupca = reverse('kupci:izmeni_kupca', args=[novi_kupac_fixture.id_kupca])
+
+        response = client.put(url_izmeni_kupca, data=test_novi_kupac, content_type='application/json')
+
+        assert response.status_code == 200
+
+    def test_serializers_izmeni_jednog_kupca_nije_validan(self,
+                                                          client,
+                                                          novi_kupac_fixture,
+                                                          novi_autorizovan_korisnik_fixture
+                                                          ):
+        """
+        Test poziv 'kupci:izmeni_kupca' za API poziv Izmeni Kupca *(NIJE VALIDAN) sa autorizovanim Korisnikom.
+        Takodje se proverava i Response sadrzaj.
+
+            * @see conftest.py (novi_autorizovan_korisnik_fixture)
+            * @see conftest.py (novi_kupac_fixture)
+
+        @param client: A Django test client instance.
+        @param novi_autorizovan_korisnik_fixture: Korisnik.
+        @param novi_kupac_fixture: Klijent (Kupac).
+        """
+
+        # Podaci za izmenu Kupca
+        test_novi_kupac = json.dumps(
+            {
+                "lice_ne_validno": "Pravno",
+                "ime_prezime_ne_validno": fake.name(),
+                "email_ne_validno": fake.email(),
+                "broj_telefona_ne_validno": "+381 63 136 90 988",
+                "Jmbg_Pib_ne_validno": "0102030605",
+                "adresa_ne_validno": "Milenke Dravica 54"
+            }
+        )
+
+        url_izmeni_kupca = reverse('kupci:izmeni_kupca', args=[novi_kupac_fixture.id_kupca])
+
+        response = client.put(url_izmeni_kupca, data=test_novi_kupac, content_type='application/json')
+
+        assert response.status_code == 400
+
+    def test_serializers_obrisi_jednog_kupca_validan(self,
+                                                     client,
+                                                     novi_kupac_fixture,
+                                                     novi_autorizovan_korisnik_fixture
+                                                     ):
+        """
+        Test poziv 'kupci:obrisi_kupca' za API poziv Obrisi Kupca *(VALIDAN) sa autorizovanim Korisnikom.
+        Takodje se proverava i Response sadrzaj.
+
+            * @see conftest.py (novi_autorizovan_korisnik_fixture)
+            * @see conftest.py (novi_kupac_fixture)
+
+        @param client: A Django test client instance.
+        @param novi_autorizovan_korisnik_fixture: Korisnik.
+        @param novi_kupac_fixture: Klijent (Kupac).
+        """
+
+        url_delete_kupca = reverse('kupci:obrisi_kupca', args=[novi_kupac_fixture.id_kupca])
+
+        response = client.delete(url_delete_kupca)
+
+        # HTTP 204 No Content success status
+        assert response.status_code == 204
+
+    def test_serializers_obrisi_jednog_kupca_nije_validan(self,
+                                                          client,
+                                                          novi_kupac_fixture,
+                                                          novi_autorizovan_korisnik_fixture
+                                                          ):
+        """
+        Test poziv 'kupci:obrisi_kupca' za API poziv Obrisi Kupca *(NIJE VALIDAN) sa autorizovanim Korisnikom.
+        Takodje se proverava i Response sadrzaj.
+
+            * @see conftest.py (novi_autorizovan_korisnik_fixture)
+            * @see conftest.py (novi_kupac_fixture)
+
+        @param client: A Django test client instance.
+        @param novi_autorizovan_korisnik_fixture: Korisnik.
+        @param novi_kupac_fixture: Klijent (Kupac).
+        """
+
+        url_delete_kupca = reverse('kupci:obrisi_kupca', args=[novi_kupac_fixture.id_kupca + 1000])
+
+        response = client.delete(url_delete_kupca)
+
+        # HTTP 404 Not Found
+        assert response.status_code == 404
