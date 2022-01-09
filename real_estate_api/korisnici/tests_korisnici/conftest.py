06ee516287bedad5dec254ccc8319d8dbf649820
Dejan Cugalj FWW <dejan.cugalj@factoryww.com>
2022-01-10 00:12:27 +0100

    [PS-84] TESTingS Korisnici:  * Create fixture  * test_kreiranje_jednog_novog_korisnika  * test_broj_novih_korisnika_u_bazi  * test_kreiranje_tri_nova_korisnika

--- real_estate_api/korisnici/tests_korisnici/conftest.py
+++ real_estate_api/korisnici/tests_korisnici/conftest.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 from faker import Faker
 
@@ -21,26 +23,128 @@ def novi_korisnik_ne_autorizovan_fixture(db) -> Korisnici:
 # endregion
 
 # region FIXTURE REGISTROVAN KORISNIK
+# @pytest.fixture(autouse=False)
+# def novi_autorizovan_korisnik_fixture(db, client, django_user_model) -> Korisnici:
+#     """
+#     Kreiranje novog Korisnika i autorizacija istog na sistem.
+#
+#     @param db: Testna DB.
+#     @param client: A Django test client instance.
+#     @param django_user_model: Korisnik.
+#     @return: Entitet autorizovan Korisnik.
+#     """
+#
+#     korisnik = Korisnici.objects.create(
+#         username='nikola',
+#         password='nikola',
+#         email='nikola@nikola.com',
+#         ime='Nikola',
+#         prezime='Nikola',
+#     )
+#
+#     client.force_login(korisnik)
+#
+#     return korisnik
+#
+#
+# # endregion
+
+# region FIXTURE REGISTROVAN JEDAN KORISNIK FULL
 @pytest.fixture(autouse=False)
-def novi_autorizovan_korisnik_fixture_kupci(db, client, django_user_model) -> Korisnici:
+def novi_jedan_auth_korisnik_fixture(db, client, django_user_model) -> Korisnici:
     """
     Kreiranje novog Korisnika i autorizacija istog na sistem.
+    Ovaj korisnik predstavlja testnog Korisnika sa popunjenim svim poljima.
+        * Ovaj Korisnik je SUPER USER.
 
     @param db: Testna DB.
     @param client: A Django test client instance.
     @param django_user_model: Korisnik.
-    @return: Entitet autorizovan Korisnik.
+    @return: Entitet autorizovan (super user) Korisnik.
     """
 
     korisnik = Korisnici.objects.create(
-        username='nikola',
+        email=fake.email(),
+        username=fake.user_name(),
         password='nikola',
-        email='nikola@nikola.com',
         ime='Nikola',
         prezime='Nikola',
+        role=Korisnici.PrivilegijeKorisnika.ADMINISTRATOR,
+        about=fake.text(max_nb_chars=25),
+        is_staff=True,
+        is_active=True,
+        is_superuser=True,
     )
 
     client.force_login(korisnik)
 
     return korisnik
+
+
+# endregion
+
+# region FIXTURE REGISTROVANA TRI KORISNIKA FULL
+@pytest.fixture(autouse=False)
+def nova_tri_korisnika_fixture(db, client, django_user_model, novi_jedan_auth_korisnik_fixture) -> Korisnici:
+    """
+    Kreiranje nova tri Korisnika i autorizacija istih na sistem.
+    Ovi korisnici predstavljaju testne Korisnike sa popunjenim svim poljima.
+        * Dejan je sa rolom ADMINISTRATOR.
+        * Slobodan je sa rolom FINANSIJE.
+        * Ivana je sa rolom PRODAVAC.
+
+    @param novi_jedan_auth_korisnik_fixture: Autorizovan Korisnik (super user).
+    @param db: Testna DB.
+    @param client: A Django test client instance.
+    @param django_user_model: Korisnik.
+    @return: Entitet autorizovan tri Korisnika.
+    """
+
+    korisnici = Korisnici.objects.bulk_create(
+        [
+            Korisnici(
+                id=1,
+                email=fake.email(),
+                username=fake.user_name(),
+                password='dejan',
+                ime='Dejan',
+                prezime='Cugalj',
+                role=Korisnici.PrivilegijeKorisnika.ADMINISTRATOR,
+                about=fake.text(max_nb_chars=25),
+                is_staff=True,
+                is_active=True,
+                is_superuser=False,
+            ),
+            Korisnici(
+                id=2,
+                email=fake.email(),
+                username=fake.user_name(),
+                password='sloba',
+                ime='Slobodan',
+                prezime='Tomic',
+                role=Korisnici.PrivilegijeKorisnika.FINANSIJE,
+                about=fake.text(max_nb_chars=25),
+                is_staff=True,
+                is_active=True,
+                is_superuser=False,
+            ),
+            Korisnici(
+                id=3,
+                email=fake.email(),
+                username=fake.user_name(),
+                password='ivana',
+                ime='Ivana',
+                prezime='Tepavac',
+                role=Korisnici.PrivilegijeKorisnika.PRODAVAC,
+                about=fake.text(max_nb_chars=25),
+                is_staff=True,
+                is_active=True,
+                is_superuser=False,
+            )
+        ]
+    )
+
+
+
+    return korisnici
 # endregion
---
+++ real_estate_api/korisnici/tests_korisnici/test_korisnici_models.py
@@ -0,0 +1,76 @@
+from real_estate_api.korisnici.models import Korisnici
+
+
+class TestEntitetaKorisnici:
+    """Testitanje entiteta Korisnici"""
+
+    def test_kreiranje_jednog_novog_korisnika(self, novi_jedan_auth_korisnik_fixture):
+        """
+        Testiranje kreiranja novog jednog Korisnika i provera podataka nakon kreiranja.
+            * @see /test_korisnici/conftest.py : novi_jedan_auth_korisnik_fixture
+
+        @param novi_jedan_auth_korisnik_fixture: entitet Korisnici
+        """
+
+        korisnik_from_db = Korisnici.objects.all()
+        email = korisnik_from_db[0].email
+        username = korisnik_from_db[0].username
+        password = korisnik_from_db[0].password
+        ime = korisnik_from_db[0].ime
+        prezime = korisnik_from_db[0].prezime
+        role = korisnik_from_db[0].role
+        about = korisnik_from_db[0].about
+        is_staff = korisnik_from_db[0].is_staff
+        is_active = korisnik_from_db[0].is_active
+        is_superuser = korisnik_from_db[0].is_superuser
+
+        assert novi_jedan_auth_korisnik_fixture.email == email
+        assert novi_jedan_auth_korisnik_fixture.username == username
+        assert novi_jedan_auth_korisnik_fixture.password == password
+        assert novi_jedan_auth_korisnik_fixture.ime == ime
+        assert novi_jedan_auth_korisnik_fixture.prezime == prezime
+        assert novi_jedan_auth_korisnik_fixture.role == role
+        assert novi_jedan_auth_korisnik_fixture.about == about
+        assert novi_jedan_auth_korisnik_fixture.is_staff == is_staff
+        assert novi_jedan_auth_korisnik_fixture.is_active == is_active
+        assert novi_jedan_auth_korisnik_fixture.is_superuser == is_superuser
+
+    def test_broj_novih_korisnika_u_bazi(self, novi_jedan_auth_korisnik_fixture):
+        """
+        Test da li je samo jedan korisnik kreiran u bazi.
+            * @see /test_korisnici/conftest.py : novi_jedan_auth_korisnik_fixture
+
+        @param novi_jedan_auth_korisnik_fixture: Korisnici
+        """
+
+        broj_korisnika_u_bazi = Korisnici.objects.all().count()
+        assert broj_korisnika_u_bazi == 1
+
+    def test_kreiranje_tri_nova_korisnika(self, nova_tri_korisnika_fixture):
+        """
+        Testiranje kreiranja nova tri Korisnika i provera podataka nakon kreiranja.
+            * @see /test_korisnici/conftest.py : nova_tri_korisnika_fixture
+
+        @param novi_korisnik_fixture: entitet Korisnici
+        """
+        korisnici = Korisnici.objects.all().values()
+        print(f'KORISNIK IZ DBa: {korisnici}')
+
+        broj_korisnika_u_bazi = Korisnici.objects.all().count()
+
+        # Ovde je 4 jer u bazi pored ova tri ima i jedn registrovan super-user Korisnik.
+        assert broj_korisnika_u_bazi == 4
+
+
+# class PasswordChange(TestCase):
+#     def test_change_password(self):
+#         c = Client()
+#         url = 'http://0.0.0.0:8000/korisnici/izmeni-korisnika/14/'
+#         payload = {'csrf_token': csrf_token, 'payload': json.dumps({'password': 'test2'})}
+#         headers = {'Cookie': 'csrftoken=' + csrf_token}
+#
+#         res = c.post(url, payload, headers=headers)
+#         res = res.json()
+#         self.assertEqual(res['password_changed'], True)
+#         korisnik = Korisnici.objects.get(username='ivana')
+#         self.assertEqual(korisnik.check_password('test2'), True)
