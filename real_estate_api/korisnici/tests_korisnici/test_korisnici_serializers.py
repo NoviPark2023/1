2a727b4fc4efd4b334b543d4197d57fec8bf9eeb
Dejan Cugalj FWW <dejan.cugalj@factoryww.com>
2022-01-11 00:44:15 +0100

    [PS-84] TESTingS Korisnici SERIALIZERS:  * Create JSON fixture  * test_serializers_svi_korisnici  * test_invalid_serializers_detalji_jednog_korisnika

--- real_estate_api/korisnici/tests_korisnici/test_korisnici_serializers.py
+++ real_estate_api/korisnici/tests_korisnici/test_korisnici_serializers.py
@@ -1,4 +1,8 @@
+import json
+
 from rest_framework.reverse import reverse
+
+from real_estate_api.korisnici.models import Korisnici
 
 
 class TestKorisniciSerijalizers:
@@ -6,15 +10,71 @@ class TestKorisniciSerijalizers:
 
     def test_serializers_svi_korisnici(self, client, nova_tri_korisnika_fixture, novi_jedan_auth_korisnik_fixture):
         """
-        Testiranje serijalizera za pregled svih Klijenata(Kupaca) sa fixturom od dva kreirana nova Kupca.
-        Testiranje se vrsi sa autorizovanim Korisnikom sistema.
+        Testiranje serijalizera za pregled svih Korisnika sa fixtures od:
+            * Tri kreirana nova obicna Korisnika.
+            * Jednog SuperUser Korisnika.
 
-            * @see conftest.py (novi_autorizovan_korisnik_fixture)
-            * @see conftest.py (nova_dva_kupaca_fixture)
+        Testiranje se vrsi sa autorizovanim Korisnikom sistema (novi_jedan_auth_korisnik_fixture).
+
+            * @see /test_korisnici/conftest.py (nova_tri_korisnika_fixture)
+            * @see /test_korisnici/conftest.py (novi_jedan_auth_korisnik_fixture)
 
         @param client: A Django test client instance.
-        @param nova_dva_kupaca_fixture: Kupci
-        @param novi_autorizovan_korisnik_fixture_kupci: Korisnik
+        @param nova_tri_korisnika_fixture: Tri obicna Korisnika
+        @param novi_jedan_auth_korisnik_fixture: SuperUser Korisnik
+        @return status code 200: HTTP OK
         """
 
-    assert True
+        response = client.get(reverse('korisnici:lista_korisnika'))
+
+        korisnici = Korisnici.objects.all().order_by('id')
+        broj_korisnika = Korisnici.objects.all().count()
+
+        assert response.status_code == 200
+
+        # Provea broja Korisnika (4).
+        # Imamo jednog SuperUser-a (novi_jedan_auth_korisnik_fixture)
+        # Imamo tri obicna Korisnika (nova_tri_korisnika_fixture)
+        assert broj_korisnika == 4
+
+        # Lokalno import 'KorisniciSerializers'.
+        from real_estate_api.korisnici.serializers import KorisniciSerializers
+
+        serializer = KorisniciSerializers(korisnici, many=True)
+
+        print(f'RESPONSE DATA: {json.dumps(serializer.data)}')
+        print(f'RESPONSE DATA: {response.json()}')
+
+        assert json.dumps(serializer.data) == json.dumps(response.json())
+
+    def test_invalid_serializers_detalji_jednog_korisnika(self,
+                                                          client,
+                                                          nova_tri_korisnika_fixture,
+                                                          novi_jedan_auth_korisnik_fixture
+                                                          ):
+        """
+        Testiranje serijalizera za pregled deatalja Korisnika koji ne postoji,
+        sa fixturom od:
+            * Tri kreirana nova obicna Korisnika.
+            * Jednog SuperUser Korisnika.
+
+        Testiranje se vrsi sa autorizovanim Korisnikom sistema (novi_jedan_auth_korisnik_fixture).
+
+            * @see /test_korisnici/conftest.py (nova_tri_korisnika_fixture)
+            * @see /test_korisnici/conftest.py (novi_jedan_auth_korisnik_fixture)
+            * @see path('detalji-korisnika/<int:id>/', KorisniciDetaljiAPIView.as_view(), name='detalji_korisnika')
+
+        ---
+        @param client: A Django test client instance.
+        @param nova_tri_korisnika_fixture: Tri obicna Korisnika
+        @param novi_jedan_auth_korisnik_fixture: SuperUser Korisnik
+        @return status code 404: HTTP 404 Not Found
+        """
+
+        # Get invalid one Korisnik ID from Response.
+        url_detalji_korisnika = reverse('korisnici:detalji_korisnika',
+                                        args=[novi_jedan_auth_korisnik_fixture.id + 1000])
+
+        response = client.get(url_detalji_korisnika)
+
+        assert response.status_code == 404
