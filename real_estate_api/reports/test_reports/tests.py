906d7ce50c6ed1fee302bdc06de1b53b2367ae8d
IvanaTepavac <icca72@gmail.com>
2022-01-12 13:26:47 +0100

    [PS-83] TEST STANOVI:  * Serializers  * Model

--- real_estate_api/reports/test_reports/tests.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from real_estate_api.stanovi.models import Stanovi
-
-
-@pytest.mark.django_db
-def test_create_category():
-    category = Stanovi.objects.create(id_stana=1)
-    assert category.id_stana == 1
