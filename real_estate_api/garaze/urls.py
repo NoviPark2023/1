3a63e57deebbcfeff5bc397d80694329518e3b68
Dejan Cugalj Fww <dejan.cugalj@factoryww.com>
2022-02-01 06:04:50 +0100

    [PS-107] Add trailnig sash on all ulrs in Garaze urls.py.

--- real_estate_api/garaze/urls.py
+++ real_estate_api/garaze/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     # Lista svih Garaza
     path('', ListaGarazaAPIView.as_view(), name='lista_garaza'),
     # Detalji Garaze
-    path('detalji-garaze/<int:id_garaze>', DetaljiGarazeAPIView.as_view(), name='detalji_garaze'),
+    path('detalji-garaze/<int:id_garaze>/', DetaljiGarazeAPIView.as_view(), name='detalji_garaze'),
     # Kreiranje Garaze
-    path('kreiraj-garazu', KreirajGarazuAPIView.as_view(), name='kreiraj_garazu'),
+    path('kreiraj-garazu/', KreirajGarazuAPIView.as_view(), name='kreiraj_garazu'),
     # Uredjivanje Garaze
-    path('izmeni-garazu/<int:id_garaze>', UrediGarazuAPIView.as_view(), name='izmeni_garazu'),
+    path('izmeni-garazu/<int:id_garaze>/', UrediGarazuAPIView.as_view(), name='izmeni_garazu'),
     # Brisanje Garaze
-    path('obrisi-garazu/<int:id_garaze>', ObrisiGarazuAPIView.as_view(), name='obrisi_garazu'),
+    path('obrisi-garazu/<int:id_garaze>/', ObrisiGarazuAPIView.as_view(), name='obrisi_garazu'),
 
 ]
