9f97b7af36a518b8cf07ca37b40c4ee426300d31
ivana <ivana.tepavac@factoryww.com>
2022-02-10 11:51:37 +0100

    [PS-121] IZVESTAJI ZA GARAZE - initiall commit (create app 'Reports Garaze')

---
+++ real_estate_api/garaze/reports_garaze/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReportsGarazeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'real_estate_api.garaze.reports_garaze'
